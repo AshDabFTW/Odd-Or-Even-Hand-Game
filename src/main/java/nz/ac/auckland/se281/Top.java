0f274c6047992dd277a8a482092d33846febaa3c
Ashkan Azarkish <aaza463@aucklanduni.ac.nz>
2024-05-08 17:02:53 +1200

    added the top strategy to the commit

---
+++ src/main/java/nz/ac/auckland/se281/Top.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland.se281;
+
+import java.util.List;
+
+public class Top implements Strategy{
+
+  @Override
+  public int computerGuess(List<Integer> previousGuesses) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'computerGuess'");
+  }
+  
+}
